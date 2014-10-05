d9f8c7ee402502d951d5f93b0fce2c64bf3fb1c6
Fifi <feefles@Evelyns-MacBook-Pro.local>
2014-10-04 22:31:19 -0400

    moved things around yay

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
  $(function() {
  	var list;
      $.getJSON('./test.json', function(data) {
-         list = new playlist.playlistView(data, 'body');
+         list = new playlist.playlistView(data, '#playlist_wrapper');
      })
 
      $('#click').click(function(e) {
