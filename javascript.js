bdf140edd6e917f858cbf44e0343e256483e62f6
Avery Crits-Christoph <avery_crits-christoph@brown.edu>
2014-10-05 00:47:28 -0400

    i fixed this again

--- javascript.js
+++ javascript.js
@@ -3,11 +3,10 @@ function resize(){
 	var size = name + 280;
 	$("#playlist_header").css("min-width", size+"px");
 	$("#playlist_wrapper").css("min-width", size+"px");
-	$("#controls").css("left", name+"px");
 }
 
 function submit(){
-	console.log("hii!");
+
 	var text = $("#new_playlist").val();
 	if ($.trim(text) != ""){
 		var newNode = document.createElement("li");
@@ -36,75 +35,99 @@ function submit(){
 	$("#addPlaylist").html("+");
 }
 
+function submit_song(){
+	$("#add").hide();
+	$("#new_song").val("");
+}
+
 $(document).ready(function() {
-  //$("table").colResizable();
-  resize();
-  var size = $("#currently_playing").width()+230;
-  $("#header").css("min-width", size+"px");
-
-  $("#friend_list li").click(function(){
-  	if (!$(this).hasClass("active")){
-  		$("#friend_list .active").removeClass("active");
-  		$(this).addClass("active");
-  	}
-  });
-
-  $("#playlist_list li").click(function(){
-  	console.log($(event.target));
-  	if (!$(this).hasClass("active") && !$(event.target).hasClass("privacy")){
-  		$("#playlist_list .active").removeClass("active");
-  		$(this).addClass("active");
-  		$("#playlist_name").text($(this).children(":first").html());
-  		resize();
-  	}
-
-  });
-
-  $(".privacy").click(function(){
-  	if ($(this).hasClass("private")){
-  		$(this).removeClass("private");
-  	}
-  	else{
-  		$(this).addClass("private");
-  	}
-  });
-
-  $("#shuffle").click(function(){
-  	if ($(this).hasClass("shuffle_on")){
-  		$(this).removeClass("shuffle_on");
-  		$(this).addClass("shuffle_off");
-  	}
-  	else {
-  		$(this).addClass("shuffle_on");
-  		$(this).removeClass("shuffle_off");
-  	}
-  });
-
-  $("#play").click(function(){
-  	if ($(this).hasClass("paused")){
-  		$(this).removeClass("paused");
-  		$(this).addClass("not_paused");
-  	}
-  	else{
-  		$(this).addClass("paused");
-  		$(this).removeClass("not_paused");
-  	}
-  })
-
-  $(window).resize(function(){
+  	//$("table").colResizable();
+  	resize();
   	var size = $("#currently_playing").width()+230;
   	$("#header").css("min-width", size+"px");
-  })
 
-  $("#addPlaylist").click(function(){
-  	$(this).html("<input id='new_playlist' type='text' name='new_playlist'>");
-	document.getElementById("new_playlist").select();
-	$("#new_playlist").keypress(function(e){
+  	$("#friend_list li").click(function(){
+  		if (!$(this).hasClass("active")){
+  			$("#friend_list .active").removeClass("active");
+  			$(this).addClass("active");
+  		}
+  	});
+
+  	$("#playlist_list li").click(function(){
+  		if (!$(this).hasClass("active") && !$(event.target).hasClass("privacy")){
+  			$("#playlist_list .active").removeClass("active");
+  			$(this).addClass("active");
+  			$("#playlist_name").text($(this).children(":first").html());
+  			resize();
+  		}
+  	});
+
+  	$(".privacy").click(function(){
+  		if ($(this).hasClass("private")){
+  			$(this).removeClass("private");
+  		}
+  		else{
+  			$(this).addClass("private");
+  		}
+ 	});
+
+  	$("#shuffle").click(function(){
+  		if ($(this).hasClass("shuffle_on")){
+  			$(this).removeClass("shuffle_on");
+  			$(this).addClass("shuffle_off");
+  		}
+  		else {
+  			$(this).addClass("shuffle_on");
+  			$(this).removeClass("shuffle_off");
+  		}
+  	});
+
+  	$("#play").click(function(){
+  		if ($(this).hasClass("paused")){
+  			$(this).removeClass("paused");
+  			$(this).addClass("not_paused");
+  		}
+  		else{
+  			$(this).addClass("paused");
+  			$(this).removeClass("not_paused");
+  		}
+  	});
+
+  	$(window).resize(function(){
+  		var size = $("#currently_playing").width()+230;
+  		$("#header").css("min-width", size+"px");
+  	});
+
+  	$("#addPlaylist").click(function(){
+  		$(this).html("<input id='new_playlist' type='text' name='new_playlist'>");
+		document.getElementById("new_playlist").select();
+		$("#new_playlist").keypress(function(e){
+			if (e.which==13){
+				submit();
+			}
+		});
+  	});
+
+  	$("#addSong").click(function(){
+  		$("#add").show();
+ 	 });
+
+  	$("#add").click(function(){
+	  	if ($(event.target).attr("id") == "add"){
+	  		$("#add").hide();
+	  	}
+  	});
+
+  	$("#new_song").keypress(function(e){
 		if (e.which==13){
-			submit();
+			submit_song();
 		}
 	});
-  });
+
+	$("#add_song_button").click(function(){
+		submit_song();
+	});
+
 
 
 });
